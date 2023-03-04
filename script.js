479209fb9a32e28dcf12aac5b2c1b91b87058dd2
Aswin <aswinkrishna363@gmail.com>
2023-03-04 20:28:09 +0530

    removed trailer showing

--- script.js
+++ script.js
@@ -67,29 +67,3 @@ const getTopRated = () => {
   fetchMovies(url, '#top_rated', 'backdrop_path')
 
 }
-
-
-// ** Fetches URL provided and returns response.json()
-async function getMovieTrailer(id) {
-  //URL: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`
-
-}
-
-
-// ** Function that adds movie data to the DOM
-const setTrailer = trailers => {
-  // Set up iframe variable to hold id of the movieTrailer Element
-  // const iframe
-  // // Set up variable to select .movieNotFound element
-  // const movieNotFound
-
-  // If there is a trailer add the src for it
-  if (trailers.length > 0) {
-    // add d-none class to movieNotFound and remove it from iframe
-
-    // add youtube link with trailers key to iframe.src
-  } else {
-    // Else remove d-none class to movieNotfound and ADD it to iframe
-
-  }
-}
